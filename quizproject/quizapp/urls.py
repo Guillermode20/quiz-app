fd20ce83912b7aaad026ab8ad81dd8957069abf1
Will Hick <william_hick@hotmail.co.uk>
2024-10-02 14:35:52 +0100

    Add Question model and admin registration
    - Added the Question model to the models.py file in the quizapp app.
    - Registered the Question model in the admin.py file.
    - This allows the Question model to be managed through the Django admin interface.

--- quizproject/quizapp/urls.py
+++ quizproject/quizapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.loadQuestions, name='home'),
+    path('restart/', views.restart, name='restart'),
 ]
--- quizproject/quizapp/views.py
+++ quizproject/quizapp/views.py
@@ -1,6 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from .models import Question
+import random
 
 # Create your views here.
+# views are functions or classes that handle web requests and return web responses
+    
+def loadQuestions(request):
+    questions = Question.objects.filter(completed=False)
+    if questions.exists():
+        selected_question = random.choice(questions)
+        question = {
+            'question_text': selected_question.question_text,
+            'option1': selected_question.option1,
+            'option2': selected_question.option2,
+            'option3': selected_question.option3,
+            'option4': selected_question.option4,
+            'completed': selected_question.completed,     
+        }
+        selected_question.completed = True
+        selected_question.save()
+        return render(request, 'quizapp/home.html', question)
+    else:
+        return render(request, 'quizapp/end.html')
 
-def home(request):
-    return render(request, 'quizapp/home.html')
+def checkAnswer(request):
+    if request.method == 'POST':
+        if 'restart' in request.POST:
+            return restart(request)
+        selected_option = request.POST.get('option')
+        if selected_option == 'option1':
+            print("Success: Option 1 was selected.")
+        elif selected_option == 'option2':
+            print("Success: Option 2 was selected.")
+        elif selected_option == 'option3':
+            print("Success: Option 3 was selected.")
+        elif selected_option == 'option4':
+            print("Success: Option 4 was selected.")
+
+def restart(request):
+    print("Success: Restart button was clicked.")
+    questions = Question.objects.all()
+    for question in questions:
+        question.completed = False
+        question.save()
+    return redirect('home')
